586395904e701fe602fbb72957cdb018a0bcb61b
Malik H <malik.h@webdevpro.net>
2023-10-02 16:41:20 +0200

    useState lorsque le composant est updated

--- jour1-tp/src/composants/Exo6.jsx
+++ jour1-tp/src/composants/Exo6.jsx
@@ -2,21 +2,35 @@ import {useState, useEffect } from "react"
 
 function Exo6() {
     const [villes, setVilles ] = useState([]);
+    const [departement, setDepartement] = useState("01")
 
     useEffect( function(){
-        fetch("https://geo.api.gouv.fr/departements/92/communes")
+        fetch(`https://geo.api.gouv.fr/departements/${departement}/communes`)
             .then(function(reponse){ return reponse.json() })
             .then(function(data){ setVilles(data)})
-    } , [])
-
+    } , [departement])
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        const departement = data.get("departement")
+        if(departement.length > 0){
+            setDepartement(departement)
+        }
+    }
     return ( 
-        <ol>
-            { villes.map(function(item, key){
-                return <li key={key}>
-                    {item.nom} - cp : {item.code} - habitant : { new Intl.NumberFormat("fr-FR").format(item.population)}
-                </li>
-            }) }
-        </ol>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="departement"/>
+                <input type="submit" />
+            </form>
+            <ol>
+                { villes.map(function(item, key){
+                    return <li key={key}>
+                        {item.nom} - cp : {item.code} - habitant : { new Intl.NumberFormat("fr-FR").format(item.population)}
+                    </li>
+                }) }
+            </ol>
+        </div>
      );
 }
 
