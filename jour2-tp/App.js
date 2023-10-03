318578e888d325f5577dfd229a7d10fefb6f0745
Malik H <malik.h@webdevpro.net>
2023-10-03 14:20:08 +0200

    premier composant

--- jour2-tp/App.js
+++ jour2-tp/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Premier from './composants/Premier';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>coucou les amis ! </Text>
+      <Premier />
       <StatusBar style="auto" />
     </View>
   );
---
+++ jour2-tp/composants/Premier.jsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native"
+
+function Premier() {
+    return ( <Text style={{ color : 'blue' , fontSize : 30 }}>mon premier composant</Text> );
+}
+
+export default Premier;
+
+// cas pratique : 
+// créer le composant Exo1
+// dans ce composant vous allez afficher un bouton qui a le texte "je suis un bouton" et qui a un fond rose
+
+// afficher ce bouton en dessous du text "mon premier composant "
