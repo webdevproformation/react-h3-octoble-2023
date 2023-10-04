2937cc5479d6acda6e627d92bb180e51fa91974a
Malik H <malik.h@webdevpro.net>
2023-10-04 15:28:44 +0200

    cas pratique

--- jour3-tp/composants/Dashboard.jsx
+++ jour3-tp/composants/Dashboard.jsx
@@ -7,3 +7,16 @@ function Dashboard({ route }) {
 }
 
 export default Dashboard;
+
+// à partir de la page d'accueil 
+// input de recherche dans lequel l'utilisateur peut saisir un pays 
+// créer un bouton qui mène vers le composant Pays
+// arriver sur le composant Pays
+// dans ce composant vous allez effectuer une requête ajax (fetch)
+// vers l'API suivant 
+// https://restcountries.com/v3.1/name/
+// afficher les informations du pays recherché dans la composant Pays 
+// nom du pays
+// sa population
+// son drapeau
+// sa capitale 
